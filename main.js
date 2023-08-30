869b76e61af70515f0696e7d4741bbb70b1ccdec
10davi <129336959+10davi@users.noreply.github.com>
2023-08-30 18:27:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+//Crie a função 'setup'
+function setup () {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+
+
+//Crie a função 'modeloLoaded'
+function modeloLoaded (){
+  console.log('PoseNet Is Initialized!');
+}
+
+
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+// Crie a função 'draw'
+function draw (){
+  background('#6C91C2');
+}
+
+
+  document.getElementById("font_size").innerHTML = "Tamanho da fonte será = " + difference +"px";
+//Utilize 'textSize'para alterar o tamanho do texto
+textSize(difference);
+fill('#FFE787');
+//Coloque o nome que aparacerá dentro do canvas
+text('canvas', 50, 400);
+
